3e7d72009af4a668506250a7dc09b71739d1dd7c
Zachary Ennenga <zsennenga@ucdavis.edu>
2013-07-18 12:57:50 -0700

    Added CowsRSS to the project

---
+++ Cows-RESTful-API/includes/Constants.php
@@ -0,0 +1,12 @@
+<?php
+define("LOGIN_PATH","Account/LogOn");
+define("EVENT_PATH","Event/Create");
+define("LOGOUT_PATH","Account/LogOff");
+define("CAS_PROXY_PATH","https://cas.ucdavis.edu:8443/cas/proxy");
+define("COWS_BASE_PATH","http://cows.ucdavis.edu/");
+define("ERROR_GENERIC", "-1");
+define("ERROR_CAS", "-2");
+define("ERROR_EVENT", "-3");
+define("ERROR_CURL", "-4");
+define("ERROR_RSS", "-5");
+?>
